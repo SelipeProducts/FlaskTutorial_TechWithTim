80c7cae4c158f15ac5d5923b59f5a8a188ca1f16
Cesar Felipe Lopez <cesarlopez200@gmail.com>
2021-03-10 00:51:47 +0000

    Handling HTTP Requests: Add method attributes GET & POST for login and sign_up functions. Next under login function store requested form data in a single variable. If request method is equal to POST assign 4 vars from request.form.get data. Add a min credential check for inputted data.

--- website/auth.py
+++ website/auth.py
@@ -1,17 +1,38 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET','POST'])
 def login():
+  data = request.form
+  print(data)
+
   return render_template('login.html', text='hello', name="Cesar", boolean=True)
 
 @auth.route('/logout')
 def logout():
   return '<p>Logout</p>'
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET','POST'])
 def sign_up():
+  if request.method == 'POST':
+    email= request.form.get('email')
+    firstname = request.form.get('firstname')
+    password1 = request.form.get('password1')
+    password2 = request.form.get('password2')
+    
+    if len(email) < 4:
+      pass
+    elif len(firstname) < 2:
+      pass
+    elif password1 != password2:
+      pass
+    elif len(password1)< 7:
+      pass
+    else:
+      #add user to db
+      pass
+
   return render_template('sign_up.html')
 
 
