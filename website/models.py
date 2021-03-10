b67b13a21e4ea230b649e7cd698e7093d5149607
Cesar Felipe Lopez <cesarlopez200@gmail.com>
2021-03-10 22:40:06 +0000

    Database Models. First imported db variable from package. Next imported UserMixin from flask_login. Next created User class with db.Model and UserMixin as extended arguments. Next created variables to represent db columns. After created Note class (Model) with its own columns. Imported func from sqlalchemy to get current time. Finally created one to many relationship between User and Note.

---
+++ website/models.py
@@ -0,0 +1,24 @@
+#from package import db var in __init__.py
+from . import db
+
+#gives user object tools for logging in
+from flask_login import UserMixin
+from sqlalchemy import func
+
+#db.Model is a blueprint of db object
+class Note(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  data = db.Column(db.String(10000))
+  date = db.Column(db.DateTime(timezone=True), default=func.now())
+  user_id = db.Column(db.Integer, db.ForeinKey('user.id'))
+
+
+class User(db.Model, UserMixin):
+  id = db.Column(db.Integer, primary_key=True)
+  email = db.Column(db.String(150), unique=True)
+  password = db.Column(db.String(150))
+  firstname = db.Column(db.String(150))
+
+  #each time user make note. Note is added to list
+  note = db.relationship('Notes')
+
