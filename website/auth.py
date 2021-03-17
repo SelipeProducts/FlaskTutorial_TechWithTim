5490d5d6c725ea2ce615d162c81b268688cc4b0d
Cesar Felipe Lopez <cesarlopez200@gmail.com>
2021-03-17 22:19:17 +0000

    Logging In Users: First in auth & in login function added if statement checking if the request method is of POST type. From form data get the email and password. Next created user var that is equal to a User in the db with the same email. After added if statement to check is user exists. Next added another if statement to check if the hashed password matches the one inputted . If login successful flash success. If wrong password entered flash error. Next added else to first if block that flashes error when email does not exists. Next in signup method added query to grab user by email after form data is retrieved. After edited if else statements by adding if statement to the beginning to test if user exists. Finally if true flash error bc email already exists.

--- website/auth.py
+++ website/auth.py
@@ -7,8 +7,23 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET','POST'])
 def login():
-  data = request.form
-  print(data)
+  # data = request.form
+  # print(data)
+  if request.method == 'POST':
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    #Query db and looking for specific user
+    user = User.query.filter_by(email=email).first()
+
+    if user:
+      if check_password_hash(user.password, password):
+        flash('Logged in successfully.', category='success')
+        return redirect(url_for('views.home'))
+      else:
+        flash('Incorrect Password.', category='error')
+    else:
+      flash('Email does not exist.', category='error')
 
   return render_template('login.html', text='hello', name="Cesar", boolean=True)
 
@@ -24,14 +39,17 @@ def sign_up():
     password1 = request.form.get('password1')
     password2 = request.form.get('password2')
     
-    if len(email) < 4:
-      flash('Email must be greater than 4 characters', category='error')
+    user = User.query.filter_by(email=email).first()
+    if user:
+      flash('Email already exists.', category='error')
+    elif len(email) < 4:
+      flash('Email must be greater than 4 characters.', category='error')
     elif len(firstname) < 2:
-      flash('Name must be greater than 2 characters', category='error')
+      flash('Name must be greater than 2 characters.', category='error')
     elif password1 != password2:
-      flash('Passwords do not match', category='error')
+      flash('Passwords do not match.', category='error')
     elif len(password1)< 7:
-      flash('Password must be greater than 7 characters', category='error')
+      flash('Password must be greater than 7 characters.', category='error')
     else:
       #add user to db
       new_user = User(email=email, firstname=firstname, password=generate_password_hash(password1, method='sha256'))
