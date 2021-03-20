a6628d8da4d232e407659f99b864b414faa16647
Cesar Felipe Lopez <cesarlopez200@gmail.com>
2021-03-20 21:37:01 +0000

    Adding User Notes:
    First imported Note, request and flash, and db
    
    Next in views.py added method attributes to view route for home function.
    
    Next in home function added if statement testing if request method is post. If true assign note var data from note form.
    
    Next added if statement checking length of note var is less then one.
    If true flash error for it being too short.
    
    After add else block thats creates a successful flash. Next in else create new_note var equal to Note class.
    
    Finally added new_note to db and
    commited changes to db.

--- website/views.py
+++ website/views.py
@@ -2,16 +2,30 @@
 #where users can go to
 #anything not related to authentication but can navigate to
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 from flask_login import login_required, current_user
-
+from .models import Note
+from . import db
 #Blueprint - has a bunch of organized routes
 views = Blueprint('views', __name__)
 
 #route holds url
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 @login_required
 def home(): 
+  if request.method == 'POST':
+    note = request.form.get('note')
+    
+    if len(note) < 1:
+      flash('Note is too short', category='error')
+    else:
+      new_note = Note(data=note, user_id= current_user.id )
+      db.session.add(new_note)
+      db.session.commit()
+
+      flash('Note added', category='success')
+
+
   #this function runs when route url is entered
   return render_template('home.html', user=current_user)
 
